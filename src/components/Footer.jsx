752be708c6f3bd3fa08ddb73bf46eb2617647baf
ikamrava <kamrava.iman@outlook.com>
2023-03-21 21:05:03 +0000

    before main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import logo from "../Logo.png"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className=' absolute bottom-5 flex items-top gap-10 bg-slate-500 w-[100%] justify-center p-2 '>
+      <span className=' h-10'><img src={logo} alt="" className=' h-[70%] w-30 '/></span>
+      <span className=' text-white pt-1'>Made with ❤️ and <b>React.js</b>.</span>
+      </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,29 @@ export default function Header() {
   return (
     <div className=' w-[100%] mt-5'>
       <div className=' flex items-center justify-between w-[100%] px-5'>
-        <div className=' h-10'><img src={logo} alt="" className=' h-[100%]'/></div>
-        <div className='hidden sm:block'>
-          <Link to="/?cat=art">
+        <div className=' h-10'><img src={logo} alt="" className=' h-[100%] w-30'/></div>
+        <div className='hidden md:block md:flex sm:gap-3 md:items-center md:justify-center'>
+          <Link className=' text-gray-700' to="/?cat=art">
             <h6>ART</h6>
           </Link>
+          <Link className=' text-gray-700' to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className=' text-gray-700'to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className=' text-gray-700'to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className=' text-gray-700'to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className=' text-gray-700' to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>User</span>
+          <span>Logout</span>
+          <span className='underline bg-slate-700 w-[50px] h-[50px] text-white rounded-full cursor-pointer text-center flex items-center justify-center hover:scale-105 '><a href="/write">Post</a></span>
         </div>
         <div className='sm:hidden'><GiHamburgerMenu/></div>
       </div>
